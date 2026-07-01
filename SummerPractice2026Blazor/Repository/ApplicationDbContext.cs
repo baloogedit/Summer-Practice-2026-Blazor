c0c7bf7d8b61680a4d52ecf79da407fa4c13bf1b
baloogedit <baloogedit@gmail.com>
2026-07-01 11:48:46 +0300

    Add project files.

---
+++ SummerPractice2026Blazor/Repository/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SummerPractice2026Blazor.Repository.Entities;
+
+namespace SummerPractice2026Blazor.Repository
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+    {
+        DbSet<ArticleCategory> ArticleCategories => Set<ArticleCategory>();
+    }
+}
